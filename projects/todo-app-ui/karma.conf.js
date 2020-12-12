9e0fb828a55f57ecf42ee06d3e5565c26514830c
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2020-12-12 22:35:36 +0000

    chore(deps-dev): Migrated from Jasmine to Mocha to have a consistent setup between UI and API

--- projects/todo-app-ui/karma.conf.js
+++ projects/todo-app-ui/karma.conf.js
@@ -4,25 +4,18 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['mocha', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-mocha'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+      mocha: {
+        opts: '../../.mocharc.yml',
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      clearContext: false // leave the Spec Runner output visible in browser
     },
     coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/todo-app-ui'),
@@ -32,7 +25,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- projects/todo-app-ui/src/app/app.component.spec.ts
+++ projects/todo-app-ui/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { expect } from 'chai';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -17,19 +18,22 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+
+    expect(app).to.be.instanceOf(AppComponent);
   });
 
   it(`should have as title 'todo-app-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-app-ui');
+
+    expect(app.title).to.equal('todo-app-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todo-app-ui app is running!');
+
+    expect(compiled.querySelector('.content span').textContent).to.include('todo-app-ui app is running!');
   });
 });
